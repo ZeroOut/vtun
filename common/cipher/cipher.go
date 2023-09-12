9fd4d64a6ffc223acc702ba6fd530b2655f62f40
ZeroOut <19900527+ZeroOut@users.noreply.github.com>
2023-09-12 09:59:22 +0800

    Update cipher.go
    动态扩展密钥长度，减少计算量，提升了网速，特别是MTU设置3000以上较为明显；稍微增加安全性（重复密文攻击）

--- common/cipher/cipher.go
+++ common/cipher/cipher.go
@@ -5,14 +5,24 @@ var _key = []byte("vtun@2022")
 
 // SetKey sets the key
 func SetKey(key string) {
-	_key = []byte(key)
+	getSha := sha512.Sum512([]byte(key))
+	_key = getSha[:]
 }
 
 // XOR encrypts the data
 func XOR(src []byte) []byte {
-	_klen := len(_key)
-	for i := 0; i < len(src); i++ {
-		src[i] ^= _key[i%_klen]
+	// Dynamic key size
+	if len(src) > len(_key) {
+		_klen := len(src) / 64
+		for i := 0; i < _klen; i++ {
+			getSha := sha512.Sum512(_key)
+			_key = append(_key, getSha[:]...)
+		}
 	}
+
+	for i := 0; i < len(src); i++ {
+		src[i] ^= _key[i]
+	}
+	
 	return src
 }
